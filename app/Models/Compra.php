16e3b0758a2d877b18a8ff7258db84e1f114dd20
CesarD23 <cesarsad090@gmail.com>
2023-07-05 05:49:39 -0600

    "creacion de tabla compras"

---
+++ app/Models/Compra.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Compra
+ *
+ * @property $id
+ * @property $nombre
+ * @property $producto
+ * @property $informacion
+ * @property $contacto
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Compra extends Model
+{
+    
+    static $rules = [
+		'nombre' => 'required',
+		'producto' => 'required',
+		'informacion' => 'required',
+		'contacto' => 'required',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nombre','producto','informacion','contacto'];
+
+
+
+}
---
+++ resources/views/compra/form.blade.php
@@ -0,0 +1,29 @@
+<div class="box box-info padding-1">
+    <div class="box-body">
+        
+        <div class="form-group">
+            {{ Form::label('nombre') }}
+            {{ Form::text('nombre', $compra->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
+            {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('producto') }}
+            {{ Form::text('producto', $compra->producto, ['class' => 'form-control' . ($errors->has('producto') ? ' is-invalid' : ''), 'placeholder' => 'Producto']) }}
+            {!! $errors->first('producto', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('informacion') }}
+            {{ Form::text('informacion', $compra->informacion, ['class' => 'form-control' . ($errors->has('informacion') ? ' is-invalid' : ''), 'placeholder' => 'Informacion']) }}
+            {!! $errors->first('informacion', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('contacto') }}
+            {{ Form::text('contacto', $compra->contacto, ['class' => 'form-control' . ($errors->has('contacto') ? ' is-invalid' : ''), 'placeholder' => 'Contacto']) }}
+            {!! $errors->first('contacto', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+
+    </div>
+    <div class="box-footer mt20">
+        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+    </div>
+</div>
