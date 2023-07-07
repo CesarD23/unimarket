416cc7d4831a17326d351ddd6b32c2108ae438d0
CesarD23 <cesarsad090@gmail.com>
2023-07-07 13:10:21 -0600

    "cambio"

--- app/Http/Controllers/CompraController.php
+++ app/Http/Controllers/CompraController.php
@@ -39,7 +39,7 @@ class CompraController extends Controller
      *
      * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
-     */
+     */ 
     public function store(Request $request)
     {
         $request->validate([
