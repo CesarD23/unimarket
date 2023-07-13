744b9eca188837fece830178bdf5b50cf3f02fcf
CesarD23 <cesarsad090@gmail.com>
2023-07-12 21:13:16 -0600

    "creacion de tarjetas"

--- resources/views/compra/index.blade.php
+++ resources/views/compra/index.blade.php
@@ -10,13 +10,13 @@
             <div class="col-sm-12">
                 <div class="card">
                     <div class="card-header">
-                        <div style="display: flex; justify-content: space-between; align-items: center;">
+                        <div class="d-flex justify-content-between align-items-center">
                             <span id="card_title">
                                 {{ __('Compra') }}
                             </span>
 
-                            <div class="float-right">
-                                <a href="{{ route('compras.create') }}" class="btn btn-primary btn-sm float-right" data-placement="left">
+                            <div class="ml-auto">
+                                <a href="{{ route('compras.create') }}" class="btn btn-primary btn-sm" data-placement="left">
                                   {{ __('Crear nuevo producto') }}
                                 </a>
                             </div>
@@ -29,49 +29,35 @@
                     @endif
 
                     <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover">
-                                <thead class="thead">
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Nombre</th>
-                                        <th>Producto</th>
-                                        <th>Informacion</th>
-                                        <th>Contacto</th>
-                                        <th>Imagen</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($compras as $compra)
-                                        <tr>
-                                            <td>{{ ++$i }}</td>
-                                            <td>{{ $compra->nombre }}</td>
-                                            <td>{{ $compra->producto }}</td>
-                                            <td>{{ $compra->informacion }}</td>
-                                            <td>{{ $compra->contacto }}</td>
-                                            <td>
-                                             <img src="{{asset($compra->imagen)}}" alt="{{$compra->title}}" class="img-fluid" width="150px">
-                                            </td>
-                                            <td>
-                                                <form action="{{ route('compras.destroy', $compra->id) }}" method="POST" >
-                                                    <a class="btn btn-sm btn-primary" href="{{ route('compras.show', $compra->id) }}">
-                                                        <i class="fa fa-fw fa-eye"></i> {{ __('Mostrar') }}
-                                                    </a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('compras.edit', $compra->id) }}">
-                                                        <i class="fa fa-fw fa-edit"></i> {{ __('Editar') }}
-                                                    </a>
+                        <div class="row">
+                            @foreach ($compras as $compra)
+                                <div class="col-md-4 mb-4">
+                                    <div class="card">
+                                        <img src="{{ asset($compra->imagen) }}" alt="{{ $compra->title }}" class="card-img-top">
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{ $compra->nombre }}</h5>
+                                            <p class="card-text">{{ $compra->producto }}</p>
+                                            <p class="card-text">{{ $compra->informacion }}</p>
+                                            <p class="card-text">{{ $compra->contacto }}</p>
+                                            <div class="d-flex justify-content-between align-items-center">
+                                                <a href="{{ route('compras.show', $compra->id) }}" class="btn btn-primary">
+                                                    <i class="fa fa-fw fa-eye"></i> {{ __('Mostrar') }}
+                                                </a>
+                                                <a href="{{ route('compras.edit', $compra->id) }}" class="btn btn-success">
+                                                    <i class="fa fa-fw fa-edit"></i> {{ __('Editar') }}
+                                                </a>
+                                                <form action="{{ route('compras.destroy', $compra->id) }}" method="POST">
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm">
+                                                    <button type="submit" class="btn btn-danger">
                                                         <i class="fa fa-fw fa-trash"></i> {{ __('Eliminar') }}
                                                     </button>
                                                 </form>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
