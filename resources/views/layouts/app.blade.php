ff84f5a36a697994c98c3b80562554508970518e
CesarD23 <cesarsad090@gmail.com>
2023-07-12 21:00:12 -0600

    "imagenes del index"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                Unimarket
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -36,16 +36,26 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
+                        @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Inicio') }}</a>
+                                </li>
+                            @endif
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Menú') }}</a>
+                                </li>
+                            @endif
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Iniciar sesion') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
                                 </li>
                             @endif
                         @else
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,51 +2,26 @@
 
 @section('content')
     <header>
-        <!-- Menú -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container">
-                <a class="navbar-brand" href="#">Mi Sitio</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Inicio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Menú</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href='login'>Iniciar Sesión</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href='register'>Registro</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+
     </header>
 
     <section class="py-5">
         <div class="container">
-            <h2>Alimentos y Comida</h2>
+           <center><h2>Unimarket </h2></center> 
             <div class="row">
                 <div class="col-md-6 col-lg-4 mb-4">
                     <div class="card">
-                        <img src="images/1689167107-1.png" class="card-img-top" alt="Food Image 1">
+                        <img src="images_welcome/food-image-1.jpg" class="card-img-top" alt="Food Image 1">
                         <div class="card-body">
                             <h5 class="card-title">Alimentos</h5>
                             <p class="card-text">Descripción del alimento 1.</p>
-                            <a href="#" class="btn btn-primary">Detalles</a>
+                            <a href='compras/' class="btn btn-primary">Detalles</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-4 mb-4">
                     <div class="card">
-                        <img src="path/to/food-image-2.jpg" class="card-img-top" alt="Food Image 2">
+                        <img src="images_welcome/food-image-2.jpg" class="card-img-top" alt="Food Image 2">
                         <div class="card-body">
                             <h5 class="card-title">Dulceria</h5>
                             <p class="card-text">Descripción del alimento 2.</p>
@@ -56,7 +31,7 @@
                 </div>
                 <div class="col-md-6 col-lg-4 mb-4">
                     <div class="card">
-                        <img src="path/to/food-image-3.jpg" class="card-img-top" alt="Food Image 3">
+                        <img src="images_welcome/food-image-3.jpg" class="card-img-top" alt="Food Image 3">
                         <div class="card-body">
                             <h5 class="card-title">Papeleria</h5>
                             <p class="card-text">Descripción del alimento 3.</p>
